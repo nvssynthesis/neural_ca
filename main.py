2ef2148e7c1eec2efa19ede8ef719485573d29e0
Nicholas Solem <nsolem@ucsd.edu>
2024-07-02 18:19:08 -0700

    fading draw

--- main.py
+++ main.py
@@ -5,17 +5,37 @@ from scipy.signal import convolve2d
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+def surf_to_normalized_array(surf: pg.Surface) -> np.ndarray:
+    array = pg.surfarray.array3d(surf)
+    array = array.astype(np.float64)
+    array *= 1/255
+    return array
+
+def normalized_array_to_surf(array: np.ndarray) -> pg.Surface:
+    array = np.clip(array, 0, 1 - 1/255)
+    array *= 255
+    # array = array.astype(np.uint8)
+    surf = pg.surfarray.make_surface(array)
+    return surf
+
 class Draw():
     def __init__(self):
-        self.last_screen = np.zeros((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.last_screen = np.zeros((SCREEN_WIDTH, SCREEN_HEIGHT, 3), dtype=np.float64)
     
     def __call__(self, screen):
         self.draw(screen)
 
-    def draw(self, screen):
-        pg.draw.rect(screen, (255, 255, 255), (100, 100, 10, 10))
+    def draw(self, screen: pg.Surface):
+        s = surf_to_normalized_array(screen) * 0.95
+        s += self.last_screen
+        s = np.clip(s, 0, 1)
+        s = normalized_array_to_surf(s)
+        screen.blit(s, (0, 0))
+        self.last_screen = surf_to_normalized_array(s) * 0.04
 
 
+def clear(screen):
+    screen.fill((0, 0, 0))
 
 def main():
     # make window appear
@@ -23,7 +43,7 @@ def main():
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pg.display.set_caption("neural worms")
 
-    screen.fill((0, 0, 0))
+    clear(screen)
 
     draw = Draw()
 
@@ -36,7 +56,7 @@ def main():
             # if space bar is clicked, clear screen
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
-                    screen.fill((0, 0, 0))
+                    clear(screen)
         
         # get mouse position
         mouse_pos = pg.mouse.get_pos()
