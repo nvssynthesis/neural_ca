642eb8f57450fb0e0dc7a40585b29be11f7f86ee
Nicholas Solem <nsolem@ucsd.edu>
2024-07-04 13:47:17 -0700

    refactoring

--- main.py
+++ main.py
@@ -1,112 +1,10 @@
 import pygame as pg
 import numpy as np
-from scipy.signal import convolve2d
 from kernel_presets import kernel_presets
+from activations import activations
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-BACKDROP_WIDTH = int(SCREEN_WIDTH * 0.8)
-BACKDROP_HEIGHT = int(SCREEN_HEIGHT * 1.0)
-
-KERNEL_DISPLAY_WIDTH = SCREEN_WIDTH - BACKDROP_WIDTH
-KERNEL_DISPLAY_HEIGHT = KERNEL_DISPLAY_WIDTH
-
-def surf_to_normalized_array(surf: pg.Surface) -> np.ndarray:
-    array = pg.surfarray.array3d(surf)
-    array = array.astype(np.float64)
-    array *= 1/255
-    return array
-
-def normalized_array_to_surf(array: np.ndarray) -> pg.Surface:
-    # array = np.clip(array, 0, 1 - 1/255)
-    array *= 255
-    # array = array.astype(np.uint8)
-    surf = pg.surfarray.make_surface(array)
-    return surf
-
-def add_surfaces(surf1: pg.Surface, surf2: pg.Surface, amt_1: float = 0.5, amt_2: float = 0.5) -> pg.Surface:
-    array1 = surf_to_normalized_array(surf1)
-    array2 = surf_to_normalized_array(surf2)
-    array = array1 * amt_1 + array2 * amt_2
-    array = np.clip(array, 0, 1)
-    return normalized_array_to_surf(array)
-
-
-def roll_surface(surf: pg.Surface, n: int=1) -> pg.Surface:
-    array = surf_to_normalized_array(surf)
-    array = np.roll(array, -n, axis=1)
-    return normalized_array_to_surf(array)
-
-def convolve_array(array: np.ndarray, base_kernel: np.ndarray) -> np.ndarray:
-    bk = base_kernel.T
-
-    kernel = np.array([
-        bk,
-        bk,
-        bk
-    ])
-    kernel[0] = kernel[0] * 1
-    kernel[1] = kernel[0] * 1
-    kernel[2] = kernel[0] * 1
-    if np.max(array) > 1:
-        breakpoint()
-    channels = []
-    for chan in range(array.shape[2]):
-        channel = array[:, :, chan]
-        # Apply convolve2d to each channel with its own kernel
-        # Assuming kernel is a list of kernels for each channel
-        convolved_channel = convolve2d(channel, kernel[0], mode='same', boundary='wrap')
-        channels.append(convolved_channel)        
-    array = np.stack(channels, axis=-1)
-    return array
-
-def apply_nonlinearity(array: np.ndarray, nonlinearity: callable) -> np.ndarray:
-    array = nonlinearity(array)
-    return array
-
-def make_terrain():
-    s = np.random.rand(13, 13, 1)
-    # make 3d array
-    s = np.concatenate([s, s, s], axis=2)
-
-    # center around 0.5
-    # s -= s.mean()
-    # s += 0.5
-    # s = np.sqrt(s)
-
-    s *= 255
-
-    s = s.astype(np.uint8)
-    s = normalized_array_to_surf(s)
-    s = s.convert(24)
-    s = pg.transform.smoothscale(s, (BACKDROP_WIDTH, BACKDROP_HEIGHT))
-    s = surf_to_normalized_array(s)
-
-    # s = np.clip(s, 0, 1)
-
-    return s
-
-
-def clear(screen):
-    screen.fill((0, 0, 0))
-
-def display_kernel(screen, kernel):
-    block_size = KERNEL_DISPLAY_WIDTH // 3
-    color = np.array([1,0,-1], dtype=np.float64)
-
-    for i in range(3):
-        for j in range(3):
-            this_color = color * kernel[i, j]
-            this_color = np.clip(this_color, 0, 1)
-            this_color *= 255
-            this_color = this_color.astype(np.uint8)
-            this_color = tuple(this_color)
-            block_surf = pg.Surface((block_size, block_size))
-            block_surf.fill(this_color)
-
-            pos_x = BACKDROP_WIDTH + j * block_size
-            pos_y = i * block_size
-            screen.blit(block_surf, (pos_x, pos_y))
+from util import SCREEN_WIDTH, SCREEN_HEIGHT, BACKDROP_WIDTH, BACKDROP_HEIGHT, \
+    surf_to_normalized_array, normalized_array_to_surf, convolve_array, display_kernel, make_terrain, clear
 
 
 def main():
@@ -124,7 +22,6 @@ def main():
     # backdrop occupies 80% of the screen and is initially all black
     backdrop = np.zeros((BACKDROP_WIDTH, BACKDROP_HEIGHT, 3), dtype=np.float64)
     backdrop = normalized_array_to_surf(backdrop)
-
 
     base_kernel = np.array([[0, 0, 0], 
                             [0, 1, 0], 
@@ -193,10 +90,7 @@ def main():
                         kernel_presets[index] = base_kernel
                     else:
                         base_kernel = kernel_presets[index]
-
-
         clear(screen)
-        # screen.blit(terrain, (0, 0))
         
         # get mouse position
         mouse_pos = pg.mouse.get_pos()
@@ -204,7 +98,6 @@ def main():
         pg.draw.circle(screen, color=(50, 128, 200), center=mouse_pos, radius=3, width=1, 
             draw_top_right=False, draw_top_left=False, draw_bottom_right=False, draw_bottom_left=False)
         
-        # hold drawing if mouse was clicked
         if pg.mouse.get_pressed()[0]:
             # draw circle on backdrop
             # random color
@@ -214,30 +107,10 @@ def main():
 
         backdrop = surf_to_normalized_array(backdrop)
         backdrop = convolve_array(backdrop, base_kernel)
-        # backdrop = backdrop + terrain_alpha * terrain
         backdrop = (1 - terrain_alpha) * backdrop + terrain_alpha * (backdrop * terrain)
-        lamdas = [
-            np.sin,     # classic yes
-            np.cos,     # crazy but sure
-            np.tanh,    # yes
 
-            # bitcrusher
-            lambda x: np.floor(x * 8) / 8,
-            #inverse gaussian
-            lambda x: -1./pow(2., (pow(x, 2.)))+1,
-
-            np.exp,     # senselessly crazy
-            np.sqrt,    # sure
-            np.arctan,  # HELL YES
-            np.arcsin,  # yes
-            np.arccos,  # no
-            np.log,     # no
-            np.abs,     # meh
-        ]
-        backdrop = apply_nonlinearity(backdrop, np.arcsin)
-        
-        # sample a line in the middle of the backdrop
-        line = backdrop[:, BACKDROP_HEIGHT // 2, :]
+        tf = np.arcsin
+        backdrop = tf(backdrop)
         
         backdrop = normalized_array_to_surf(backdrop)
 
